22254ec7d3677d7de4fcaceb132663ef63514d50
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-09 11:52:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student < User
+  def initialize
+    @name = name
+    @learn = learn
+    @knowledge = knowledge
+  end
 
+  def learn
+    knowledge = []
+  end
+
+  def knowledge
+    self.knowledge
+  end
 end
