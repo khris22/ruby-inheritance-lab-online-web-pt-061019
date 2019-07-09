0b82d15495d398361ebdd4dd57e16d309cad0610
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-09 11:56:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 class Student < User
-  # def initialize
-  #   # @name = name
-  #   @learn = learn
-  #   @knowledge = knowledge
-  # end
+  def initialize
+    # @name = name
+    # @learn = learn
+    @knowledge = []
+  end
 
   def learn
     knowledge = []
